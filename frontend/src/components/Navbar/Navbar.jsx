bdc5fd416972707251c4e70c8aa7d56967a34957
JakubStankiewicz1 <272264@student.pwr.edu.pl>
2025-02-09 20:50:02 +0100

    Navbar responsivity

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import assets from '../../assets/assets';
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
         <div className="navbarContainer">
             <div className="navbarLeftPart">
                 <Link to="/" className="navbarLeftPartName snowburst-one-regular">Choinek.pl</Link>
             </div>
-            <div className="navbarMiddlePart">
+            <div className="navbarToggle" onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <div className={`navbarMiddlePart ${menuOpen ? 'active' : ''}`}>
                 <p className="navbarMiddlePartElement"><NavLink to="/products">oferta</NavLink><div className="navbarMiddlePartUnderline" /></p>
                 <p className="navbarMiddlePartElement">cennik<div className="navbarMiddlePartUnderline" /></p>
                 <NavLink to="/our-realizations" className="navbarMiddlePartElement">nasze realizacje<div className="navbarMiddlePartUnderline" /></NavLink>
                 <NavLink to="/faq" className="navbarMiddlePartElement">faq<div className="navbarMiddlePartUnderline" /></NavLink>
-                {/* <p className="navbarMiddlePartElement">zamów online<div className="navbarMiddlePartUnderline" /></p> */}
                 <div className="navbarMiddlePartElement">
                     <img src={assets.shoppingCart} alt="" className="navbarMiddlePartElementImg" />
                     <div className="navbarMiddlePartUnderline" />
